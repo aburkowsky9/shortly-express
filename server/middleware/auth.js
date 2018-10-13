08d333685057340cb2fc83cdd076acdab9444942
aburkowsky9 <aburkowsky9@gmail.com>
2018-10-12 21:17:50 -0700

    Implements cookie parser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,23 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  if (!Object.keys(req.cookies).length) {
+    models.Sessions.create()
+    .then(data => {
+      console.log(data);
+      models.Sessions.get({id: 1});
+    })
+    .then(data => {
+      console.log(data);
+    })
+    next();
+  }
+  // find shortly id hash
+  // query sessions db with this hash
+  // if valid
+    // get user id
+    // add user id to session 
+  // else
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,16 @@
 const parseCookies = (req, res, next) => {
+  var cookie = req.headers.cookie;
+  if (!cookie) {
+    next();
+  } 
+  else {
+    var cookiesArr = cookie.split('; ');
+    cookiesArr.forEach((item) => {
+      var cookieSplit = item.split('=');
+      req.cookies[cookieSplit[0]] = cookieSplit[1];
+    })
+    next();
+  }  
 };
 
 module.exports = parseCookies;
